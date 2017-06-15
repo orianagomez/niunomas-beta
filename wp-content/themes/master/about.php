3ecdaeabb12e57457489cda2f6c2be3627d3c69a
Oriana Gómez <orianagomez@gmail.com>
2017-06-15 19:05:00 -0400

    Estilo y contenido single

--- wp-content/themes/master/about.php
+++ wp-content/themes/master/about.php
@@ -3,7 +3,9 @@
 <div class="container text-center">
 	<?php while ( have_posts() ) : the_post(); ?>
 		<h1><?php the_title(); ?></h1>
-		<div class="content"><?php the_content(); ?></div>
+		<div class="content">
+			<?php the_content(); ?>
+		</div>
 	<?php endwhile; ?>
 </div>
 
--- wp-content/themes/master/single.php
+++ wp-content/themes/master/single.php
@@ -1,9 +1,17 @@
 <?php get_header(); ?>
 <div class="container text-center">
 	<?php while ( have_posts() ) : the_post(); ?>
-		<div><?php the_post_thumbnail('full'); ?></div>
-		<h1><?php the_title(); ?></h1>
-		<div class="content"><?php the_content(); ?></div>
+		<div class="col-lg-6 text-right">
+			<h1><?php the_title(); ?></h1>
+			<div class="content">
+				<p><?php echo CFS()->get('edad').". ".CFS()->get('ciudad');?></p>
+				<p><?php echo CFS()->get('fecha_nac');?></p>
+				<?php the_content(); ?>
+			</div>
+		</div>
+		<div class="col-lg-6 pic">
+			<div><?php the_post_thumbnail('full'); ?></div>
+		</div>
 	<?php endwhile; ?>
 </div>
 
