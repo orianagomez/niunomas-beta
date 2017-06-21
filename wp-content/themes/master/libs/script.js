891bb6b70224671a90fb8229c5435c99c83f9301
Oriana Gómez <orianagomez@gmail.com>
2017-06-21 18:02:45 -0400

    Animacion scroll header

--- wp-content/themes/master/libs/script.js
+++ wp-content/themes/master/libs/script.js
@@ -1,75 +1,49 @@
 (function( $ ) {
 	'use strict';
 
-		/*var elSelector		= 'header',
-			elClassHidden	= 'header--hidden',
-			throttleTimeout	= 500,
-			element			= document.querySelector( elSelector );
-
-		if( !element ) return true;
-
-		var dHeight			= 0,
-			wHeight			= 0,
-			wScrollCurrent	= 0,
-			wScrollBefore	= 0,
-			wScrollDiff		= 0,
-
-			hasElementClass		= function( element, className ){ return element.classList ? element.classList.contains( className ) : new RegExp( '(^| )' + className + '( |$)', 'gi' ).test( element.className ); },
-			addElementClass		= function( element, className ){ element.classList ? element.classList.add( className ) : element.className += ' ' + className; },
-			removeElementClass	= function( element, className ){ element.classList ? element.classList.remove( className ) : element.className = element.className.replace( new RegExp( '(^|\\b)' + className.split( ' ' ).join( '|' ) + '(\\b|$)', 'gi' ), ' ' ); },
-
-			throttle = function( delay, fn )
-			{
-				var last, deferTimer;
-				return function()
-				{
-					var context = this, args = arguments, now = +new Date;
-					if( last && now < last + delay )
-					{
-						clearTimeout( deferTimer );
-						deferTimer = setTimeout( function(){ last = now; fn.apply( context, args ); }, delay );
-					}
-					else
-					{
-						last = now;
-						fn.apply( context, args );
-					}
-				};
-			};
-
-		console.log('get in');
-
-		window.addEventListener( 'scroll', throttle( throttleTimeout, function()
-		{
-			dHeight			= document.body.offsetHeight;
-			wHeight			= window.innerHeight;
-			wScrollCurrent	= window.pageYOffset;
-			wScrollDiff		= wScrollBefore - wScrollCurrent;
-
-			console.log(wScrollCurrent);
-
-			if( wScrollCurrent <= 0 ) // scrolled to the very top; element sticks to the top
-				addElementClass( element, elClassHidden );
-
-			else if( wScrollDiff > 0 && hasElementClass( element, elClassHidden ) ) // scrolled up; element slides in
-				removeElementClass( element, elClassHidden );
-
-			else if( wScrollDiff < 0 ) // scrolled down
-			{
-				if( wScrollCurrent + wHeight >= dHeight && hasElementClass( element, elClassHidden ) ) // scrolled to the very bottom; element slides in
-					removeElementClass( element, elClassHidden );
-
-				else // scrolled down; element slides out
-					addElementClass( element, elClassHidden );
-			}
-
-			wScrollBefore = wScrollCurrent;
-		}));*/
-
-		$(document).ready(function(){
-			$('.navbar-toggle').click(function(){
-				$(this).toggleClass('open');
-			});
+	$(document).ready(function(){
+		$('.navbar-toggle').click(function(){
+			$(this).toggleClass('open');
 		});
+
+				// Hide Header on on scroll down
+		var didScroll;
+		var lastScrollTop = 0;
+		var delta = 2;
+		var navbarHeight = $('header').outerHeight();
+
+		$(window).scroll(function(event){
+		    didScroll = true;
+		});
+
+		setInterval(function() {
+		    if (didScroll) {
+		        hasScrolled();
+		        didScroll = false;
+		    }
+		}, 250);
+
+		function hasScrolled() {
+		    var st = $(document).scrollTop();
+		    		    
+		    // Make sure they scroll more than delta
+		    if(Math.abs(lastScrollTop - st) <= delta)
+		        return;
+		    
+		    // If they scrolled down and are past the navbar, add class .nav-up.
+		    // This is necessary so you never see what is "behind" the navbar.
+		    if (st > lastScrollTop && st > navbarHeight){
+		        // Scroll Down
+		        $('header').removeClass('nav-down').addClass('nav-up');
+		    } else {
+		        // Scroll Up
+		        if(st + $(window).height() < $(document).height()) {
+		            $('header').removeClass('nav-up').addClass('nav-down');
+		        }
+		    }
+		    
+		    lastScrollTop = st;
+		}
+	});
 
 })( jQuery );
